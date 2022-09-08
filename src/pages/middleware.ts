e4f871a15d9ca7a25d576472406ff69ee55c7c2f
sigespweb22 <sigespweb22@gmail.com>
2022-09-08 08:39:54 -0300

    clean code

--- src/pages/middleware.ts
+++ src/pages/middleware.ts
@@ -4,8 +4,6 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    debugger;
-    
     const response = NextResponse.next()
     response.cookies.set('vercel', 'fast')
     response.cookies.set('vercel', 'fast', { path: '/test' })
