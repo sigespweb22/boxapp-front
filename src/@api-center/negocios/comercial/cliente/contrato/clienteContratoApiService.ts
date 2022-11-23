68b41e96145da4a5bd748f6fc1a838e1cd213cc5
sigespweb22 <sigespweb22@gmail.com>
2022-11-23 13:29:40 -0300

    ajustes merge

--- src/@api-center/negocios/comercial/cliente/contrato/clienteContratoApiService.ts
+++ src/@api-center/negocios/comercial/cliente/contrato/clienteContratoApiService.ts
@@ -3,6 +3,8 @@ import env from '../../../../../environment';
 const apiClientesContratos = `${env.API_URL}/clientes-contratos`
 
 export default {
+  sincronizarFromThirdPartyAsync: `${apiClientesContratos}/sincronizar-from-third-party/`,
+  updatePeriodicidadeFromThirdPartyAsync: `${apiClientesContratos}/update-periodicidade-from-third-party/`,
   listAsync: `${apiClientesContratos}/list/`,
   listOneAsync: `${apiClientesContratos}/list-one/`,
   listToSelectAsync: `${apiClientesContratos}/list-to-select/`,
