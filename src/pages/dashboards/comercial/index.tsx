b200a4169310bff7da16415e5e09336784b3b196
sigespweb22 <sigespweb22@gmail.com>
2022-11-23 09:24:38 -0300

    cards ticket médio on issue #24

--- src/pages/dashboards/comercial/index.tsx
+++ src/pages/dashboards/comercial/index.tsx
@@ -14,9 +14,11 @@ import CardStatisticsVerticalComponent from 'src/@core/components/card-statistic
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
 
 // ** Demo Components Imports
+import BoasVindas from 'src/views/dashboards/comercial/BoasVindas'
 import ClientesContratosNumerosChart from 'src/views/dashboards/comercial/ClientesContratosNumerosChart'
 import ClientesContratosValoresChart from 'src/views/dashboards/comercial/ClientesContratosValoresChart'
-import BoasVindas from 'src/views/dashboards/comercial/BoasVindas'
+import ClientesContratosTicketMedioMensalChart from 'src/views/dashboards/comercial/ClientesContratosTicketMedioMensalChart'
+import ClientesContratosTicketMedioAnualChart from 'src/views/dashboards/comercial/ClientesContratosTicketMedioAnualChart'
 
 const EcommerceDashboard = () => {
   return (
@@ -24,6 +26,20 @@ const EcommerceDashboard = () => {
       <Grid container spacing={6}>
         <Grid item xs={12} md={12} sx={{ order: 0, alignSelf: 'flex-end' }}>
           <BoasVindas />
+        </Grid>
+        <Grid item xs={12} md={6} lg={6} sx={{ order: 0 }}>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <ClientesContratosTicketMedioMensalChart />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6} lg={6} sx={{ order: 0 }}>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <ClientesContratosTicketMedioAnualChart />
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={12} md={6} lg={6} sx={{ order: 0 }}>
           <Grid container spacing={6}>
---
+++ src/views/dashboards/comercial/ClientesContratosTicketMedioMensalChart.tsx
@@ -0,0 +1,201 @@
+import { SetStateAction, useEffect, useState, useContext } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import { useTheme } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
+
+// ** Icons Imports
+import ChartTimelineVariant from 'mdi-material-ui/ChartTimelineVariant'
+import FormatAlignMiddle from 'mdi-material-ui/FormatAlignMiddle'
+
+// ** Third Party Imports
+import { ApexOptions } from 'apexcharts'
+
+// ** Custom Components Importss
+import CustomAvatar from 'src/@core/components/mui/avatar'
+
+// ** Util Import
+import { hexToRGBA } from 'src/@core/utils/hex-to-rgba'
+
+// ** Import Api services
+import dashboardApiServices from 'src/@api-center/dashboards/comercial/dashboardComercialApiService'
+
+// ** Import Axios
+import axios from 'axios'
+
+// ** Toast
+import toast from 'react-hot-toast'
+
+// ** Context Imports
+import { AbilityContext } from 'src/layouts/components/acl/Can'
+import Divider from '@mui/material/Divider'
+
+interface ClienteContratoValor {
+  totalEmReaisTodosOsContratos: number
+  totalEmReaisContratosMensais: number
+  totalEmReaisContratosAnuais: number
+}
+
+const clientesContratosValoresDefaultValues: ClienteContratoValor = {
+  totalEmReaisTodosOsContratos: 0,
+  totalEmReaisContratosMensais: 0,
+  totalEmReaisContratosAnuais: 0
+}
+
+const calcularPercentualValorMensalEmRelacaoAoValorTotal = (data: ClienteContratoValor) => {
+  if (data.totalEmReaisContratosMensais > 0 && data.totalEmReaisTodosOsContratos > 0)
+  {
+    const calculo = (data.totalEmReaisContratosMensais / data.totalEmReaisTodosOsContratos) * 100
+    return calculo
+  } else return 0
+}
+
+const calcularPercentualValorAnualEmRelacaoAoValorTotal = (data: ClienteContratoValor) => {
+  if (data.totalEmReaisContratosAnuais > 0 && data.totalEmReaisTodosOsContratos > 0)
+  {
+    const calculo = (data.totalEmReaisContratosAnuais / data.totalEmReaisTodosOsContratos) * 100
+    return calculo
+  } else return 0
+}
+
+const formatToCurrency = (value: number) => {
+  return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+}
+
+const ClientesContratosTicketMedioMensalChart = () => {
+  // ** Hook
+  const theme = useTheme()
+  const ability = useContext(AbilityContext)
+
+  // ** State
+  const [clientesContratosValores, setClientesContratosValores] = useState<ClienteContratoValor>(clientesContratosValoresDefaultValues)
+  const [percentualValorMensalEmRelacaoAoValorTotal, setPercentualValorMensalEmRelacaoAoValorTotal] = useState(0)
+  const [percentualValorAnualEmRelacaoAoValorTotal, setPercentualValorAnualEmRelacaoAoValorTotal] = useState(0)
+
+  const accessToken = window.localStorage.getItem(dashboardApiServices.storageTokenKeyName);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }
+
+  useEffect(() => {
+    const response = axios.get(dashboardApiServices.clientesContratosValoresAsync, config)
+    
+    response.then((response: { data: ClienteContratoValor }): void => {
+      setPercentualValorMensalEmRelacaoAoValorTotal(calcularPercentualValorMensalEmRelacaoAoValorTotal(response.data))
+      setPercentualValorAnualEmRelacaoAoValorTotal(calcularPercentualValorAnualEmRelacaoAoValorTotal(response.data))
+      setClientesContratosValores(response.data)
+    }).catch((error: { response: { data: { errors: { [s: string]: unknown } | ArrayLike<unknown> } } }): void => {
+      const returnObj = Object.entries(error.response.data.errors);
+      returnObj.forEach((err: any) => {
+        toast.error(err)
+      })
+    })
+  }, [])
+
+  const options: ApexOptions = {
+    chart: {
+      parentHeightOffset: 0,
+      toolbar: { show: false }
+    },
+    grid: {
+      show: false,
+      padding: {
+        top: -5,
+        left: -10,
+        right: -7,
+        bottom: -12
+      }
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        distributed: true,
+        columnWidth: '60%',
+        endingShape: 'rounded',
+        startingShape: 'rounded'
+      }
+    },
+    legend: { show: false },
+    dataLabels: { enabled: false },
+    colors: [
+      hexToRGBA(theme.palette.success.main, 0.1),
+      theme.palette.success.main,
+      hexToRGBA(theme.palette.success.main, 0.1),
+      hexToRGBA(theme.palette.success.main, 0.1),
+      hexToRGBA(theme.palette.success.main, 0.1),
+      hexToRGBA(theme.palette.success.main, 0.1),
+    ],
+    states: {
+      hover: {
+        filter: { type: 'none' }
+      },
+      active: {
+        filter: { type: 'none' }
+      }
+    },
+    xaxis: {
+      tickPlacement: 'OF',
+      labels: { show: false },
+      axisTicks: { show: false },
+      axisBorder: { show: false },
+      categories: ['S', 'M', 'T', 'W', 'T', 'F']
+    },
+    yaxis: { show: false }
+  }
+
+  return (
+    <Card>
+      <Divider />
+      <Grid container spacing={12}>
+        <Grid item xs={12} md={12} lg={12} sx={{ order: 0 }}>
+          <CardContent>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Box sx={{ mr: 0, display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <CustomAvatar skin='light' sx={{ mr: 4, width: 42, height: 42 }} variant='rounded' color='info'>
+                    <FormatAlignMiddle sx={{ fontSize: '1.875rem', color: 'info.main' }} />
+                  </CustomAvatar>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', mr: 4 }}>
+                    <Typography variant="h5" sx={{ fontWeight: 600 }}>Ticket Médio (R$) | Mensal</Typography>
+                  </Box>
+                  <CustomAvatar skin='light' sx={{ mr: 4, width: 42, height: 42 }} variant='rounded' color='info'>
+                    <FormatAlignMiddle sx={{ fontSize: '1.875rem', color: 'info.main' }} />
+                  </CustomAvatar>
+                </Box>
+                <Typography component='p' variant='caption' sx={{ mt: 12 }}>
+                  Valor total dos contratos mensais ativos - em real - 
+                </Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Typography variant='h6'>
+                    {formatToCurrency(clientesContratosValores.totalEmReaisContratosMensais)}
+                  </Typography>
+                  <ChartTimelineVariant sx={{ color: 'secondary.main', ml: 2, mr: 2 }} />
+                  <Typography variant='caption' sx={{ color: 'info.main' }}>
+                    {Math.round(percentualValorMensalEmRelacaoAoValorTotal)}% em relação ao total dos contratos
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </CardContent>
+        </Grid>
+        <Divider />
+      </Grid>
+      <Divider />
+    </Card>
+  )
+}
+
+// ** Controle de acesso da página
+// ** Usuário deve possuir a habilidade para ter acesso a esta página
+ClientesContratosTicketMedioMensalChart.acl = {
+  action: 'read',
+  subject: 'ac-dashboard-comercial-page'
+}
+
+export default ClientesContratosTicketMedioMensalChart
